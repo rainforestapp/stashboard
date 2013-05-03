5cc6dac87219835c3ac5834c0fc500f78a50b454
Russell Smith <russell.smith@ukd1.co.uk>
2013-05-02 17:33:41 -0700

    Make up some oauth secrets, update the email address

--- stashboard/settings.py
+++ stashboard/settings.py
@@ -5,21 +5,21 @@ DEBUG = False
 SITE_NAME = "Rainforest QA Status"
 SITE_AUTHOR = "Russell Smith"
 SITE_URL = "http://status.rainforestqa.com"
-REPORT_URL = "mailto:hello@rainforestqa.com"
+REPORT_URL = "mailto:team@rainforestqa.com"
 
 # Twitter update settings
 TWITTER_CONSUMER_KEY = ''
 TWITTER_CONSUMER_SECRET = ''
 TWITTER_ACCESS_TOKEN = ''
 TWITTER_ACCESS_TOKEN_SECRET = ''
-TWITTER_HANDLE = 'stashboard_status'
+TWITTER_HANDLE = 'rainforestqa'
 
 # RSS Feed settings
 RSS_NUM_EVENTS_TO_FETCH = 50
 
 # OAuth Consumer Credentials
-CONSUMER_KEY = 'anonymous'
-CONSUMER_SECRET = 'anonymous'
+CONSUMER_KEY = '4dba98dcc2b271291ff337c7c655a679'
+CONSUMER_SECRET = '979e48e67ef0c406bc1762beb9935103962dec8a'
 
 TEMPLATE_DIRS = (
     os.path.join(os.path.dirname(__file__), "templates"),
